85495f193d0acd4796caece934c0f96f96482a26
paristuzun <paristuzun@gmail.com>
2019-07-13 15:26:43 -0400

    added react app

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+
+    resources :stories
+    get '/stories', to: 'stories#index'
+    get '/topStories', to: 'stories#topStories'
+    get '/channels', to: 'stories#channels'
+    get '/channels/:channel', to: 'stories#channel_stories'
+    post'/stories_search', to: 'stories#stories_search'
+    get '/searches', to: 'search#last_ten'
+    post '/searches/:id', to: 'search#update'
+
+
+
 end
